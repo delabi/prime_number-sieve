ee76a0fe47342a0557fce33e229bae1fe8b5e20a
delabi <bwire.laban@gmail.com>
2017-04-04 12:31:26 +0300

    Additions to the tests

--- test_prime_generator.py
+++ test_prime_generator.py
@@ -5,5 +5,11 @@ class PrimeNumberGenerator(unittest.TestCase):
 	def test_generate_prime_numbers_returns_prime_numbers(self):
 		self.assertEqual(generate_prime_numbers(13), [2, 3, 5, 7, 11, 13], "Fail: The function is not working!")
 
+	def test_generate_prime_numbers_rejects_negative_numbers(self):
+		self.assertEqual(generate_prime_numbers(-10), "Please enter a positive integer", "Fail: No handling of negative numbers!")
+
+	def test_generate_prime_numbers_checks_for_integers_only(self):
+		self.assertEqual(generate_prime_numbers(53.34), "That is not an integer. Please enter a number without a decimal", "Fail: No handling of non integers like decimals")
+
 if __name__ == '__main__':
 	unittest.main()
