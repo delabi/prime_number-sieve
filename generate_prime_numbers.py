2f9ef18852c6c92a1e16ed3548f9ec4fdfe7223c
delabi <bwire.laban@gmail.com>
2017-04-04 11:32:29 +0300

    initial commit of the code

---
+++ generate_prime_numbers.py
@@ -0,0 +1,25 @@
+def generate_prime_numbers(n):
+	if n == 0:
+		return[0]
+	elif n == 2: 
+		return[2]
+	elif n < 2: 
+		return []
+	s = range (3, n+1, 2)
+	mroot = n ** 0.5
+	half = (n+1)/2-1
+	i = 0
+	m = 3
+	while  m <= mroot:
+		if s[i]:
+			j = (m*m-3)/2
+			s[j] = 0
+			while j < half:
+				s[j] = 0
+				j+=m
+		i = i + 1
+		m = 2 * i +3
+	return [0, 2] + [x for x in s if x]
+
+print generate_prime_numbers(13)
+print generate_prime_numbers(2000000)
